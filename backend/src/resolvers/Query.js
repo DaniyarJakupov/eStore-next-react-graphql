c75ebfe6db5b4cd564887fcf64554f4889b28fd7
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-03 12:03:58 +0200

    Add query to fetch all items

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,18 +1,11 @@
 const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  me(parent, args, ctx, info) {
-    // check if there is a current user ID
-    if (!ctx.request.userId) {
-      return null;
-    }
-    return ctx.db.query.user(
-      {
-        where: { id: ctx.request.userId }
-      },
-      info
-    );
-  }
+  //   async items(parent, args, ctx, info) {
+  //     const items = await ctx.db.query.items();
+  //     return items;
+  //   }
+  items: forwardTo("db")
 };
 
 module.exports = Query;
