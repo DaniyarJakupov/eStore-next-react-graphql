2280cad915c979f8ed21df2501836b1dfb70bc4e
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-03 12:03:46 +0200

    Add createItem resolver

--- backend/src/resolvers/Mutations.js
+++ backend/src/resolvers/Mutations.js
@@ -2,31 +2,12 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const Mutations = {
-  async signup(parent, args, ctx, info) {
-    // lowercase their email
-    args.email = args.email.toLowerCase();
-    // hash their password
-    const password = await bcrypt.hash(args.password, 10);
-    // create the user in the database
-    const user = await ctx.db.mutation.createUser(
-      {
-        data: {
-          ...args,
-          password,
-          permissions: { set: ["USER"] }
-        }
-      },
-      info
-    );
-    // create the JWT token for them
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-    // We set the jwt as a cookie on the response
-    ctx.response.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
-    });
-    // Finalllllly we return the user to the browser
-    return user;
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if user auth-ed
+
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+
+    return item;
   }
 };
 
