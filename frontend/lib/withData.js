6fc5a201c4260c5ef36890d15b90c048fad17919
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-03 12:48:11 +0200

    Setup Apollo client

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,18 +1,18 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from "next-with-apollo";
+import ApolloClient from "apollo-boost";
+import { endpoint } from "../config";
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
-          credentials: 'include',
+          credentials: "include"
         },
-        headers,
+        headers
       });
-    },
+    }
   });
 }
 
