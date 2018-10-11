3d3872320a6706d3d1060bc0efae06841f20718c
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-11 10:47:25 +0200

    Query graphql server for all items

---
+++ frontend/components/Items.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+
+import Item from "./Item";
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      image
+      largeImage
+      description
+      price
+    }
+  }
+`;
+
+const RootWrapper = styled.div`
+  text-align: center;
+`;
+const ItemsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+class Items extends Component {
+  state = {};
+  render() {
+    return (
+      <RootWrapper>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ error, loading, data }) => {
+            if (error) return <p>Error {error.message}</p>;
+            if (loading || !data) return <p>Fetching...</p>;
+
+            return (
+              <ItemsWrapper>
+                {data.items.map(item => {
+                  return <Item item={item} key={item.id} />;
+                })}
+              </ItemsWrapper>
+            );
+          }}
+        </Query>
+      </RootWrapper>
+    );
+  }
+}
+
+export default Items;
