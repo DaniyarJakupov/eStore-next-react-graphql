9ac3c25b17cb330c05126f8fca70a527333d2824
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-11 10:47:38 +0200

    Render an Item

---
+++ frontend/components/Item.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import Title from "./styles/Title";
+import ItemStyles from "./styles/ItemStyles";
+import PriceTag from "./styles/PriceTag";
+import formatMoney from "../lib/formatMoney";
+
+const Item = ({ item }) => {
+  return (
+    <ItemStyles>
+      {item.image && <img src={item.image} alt={item.title} />}
+      <Title>
+        <Link href={{ pathname: "/item", query: { id: item.id } }}>
+          <a>{item.title}</a>
+        </Link>
+      </Title>
+      <PriceTag>{formatMoney(item.price)}</PriceTag>
+      <p>{item.description}</p>
+
+      <div className="buttonList">
+        <Link href={{ pathname: "update", query: { id: item.id } }}>
+          <a>Edit</a>
+        </Link>
+
+        <button>Add to cart</button>
+
+        <button>Delete</button>
+      </div>
+    </ItemStyles>
+  );
+};
+
+export default Item;
