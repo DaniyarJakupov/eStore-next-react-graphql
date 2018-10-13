e3547dbbd07cb8815c6991bc3950dcbe0d15a3ff
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-13 11:38:14 +0200

    Add updateItem mutation

--- backend/src/resolvers/Mutations.js
+++ backend/src/resolvers/Mutations.js
@@ -4,10 +4,19 @@ const jwt = require("jsonwebtoken");
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if user auth-ed
-
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
-
     return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    // Take a copy of updated fields
+    const updates = { ...args };
+    // Remove ID from the updates
+    delete updates.id;
+    // Run update method
+    return ctx.db.mutation.updateItem(
+      { data: { ...updates }, where: { id: args.id } },
+      info
+    );
   }
 };
 
