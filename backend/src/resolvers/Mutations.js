6f122d5ecbb2890b5c18c8dc64d857410046b899
Daniyar Jakupov <ddr199305@gmail.com>
2018-10-18 12:15:28 +0200

    Add deleteItem mutation

--- backend/src/resolvers/Mutations.js
+++ backend/src/resolvers/Mutations.js
@@ -17,6 +17,15 @@ const Mutations = {
       { data: { ...updates }, where: { id: args.id } },
       info
     );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. Find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // 2. Check permissions
+    // TODO
+    // 3. Delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
